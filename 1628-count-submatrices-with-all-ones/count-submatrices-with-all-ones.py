289f189d7b30f32d80a6eb5f2b911e1a3a3d5b41
310623104067 <310623104067@eec.srmrmp.edu.in>
2026-02-06 08:44:50 +0530

    Time: 58 ms (64.29%) | Memory: 13 MB (20.41%) - LeetSync

---
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def numSubmat(self, mat):
+        m = len(mat)
+        n = len(mat[0])
+        heights = [0] * n
+        ans = 0
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    heights[j] = 0
+                else:
+                    heights[j] += 1
+
+            stack = []
+            count = [0] * n
+
+            for j in range(n):
+                while stack and heights[stack[-1]] >= heights[j]:
+                    stack.pop()
+
+                if stack:
+                    prev = stack[-1]
+                    count[j] = count[prev] + heights[j] * (j - prev)
+                else:
+                    count[j] = heights[j] * (j + 1)
+
+                stack.append(j)
+                ans += count[j]
+
+        return ans
