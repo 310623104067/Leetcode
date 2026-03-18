aff2d8894fbb265a66f0416bd74edfc2af509279
310623104067 <310623104067@eec.srmrmp.edu.in>
2026-03-18 22:43:12 +0530

    Time: 87 ms (78.20%) | Memory: 14.4 MB (74.41%) - LeetSync

---
+++ 772-construct-quad-tree/construct-quad-tree.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def construct(self, grid):
+        
+        def build(r, c, size):
+            first = grid[r][c]
+            same = True
+            
+            for i in range(r, r + size):
+                for j in range(c, c + size):
+                    if grid[i][j] != first:
+                        same = False
+                        break
+                if not same:
+                    break
+            
+            if same:
+                return Node(first == 1, True, None, None, None, None)
+            
+            half = size // 2
+            
+            topLeft = build(r, c, half)
+            topRight = build(r, c + half, half)
+            bottomLeft = build(r + half, c, half)
+            bottomRight = build(r + half, c + half, half)
+            
+            return Node(True, False, topLeft, topRight, bottomLeft, bottomRight)
+        
+        return build(0, 0, len(grid))
