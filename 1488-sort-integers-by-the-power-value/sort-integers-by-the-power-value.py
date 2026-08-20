1c3c650822c7436c9dd5b61d8f2f14f751957fc2
Karthik Srinivas Sekar <310623104067@eec.srmrmp.edu.in>
2026-08-20 09:16:37 +0530

    Time: 379 ms (34.09%) | Memory: 12.4 MB (86.36%) - LeetSync

---
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -0,0 +1,23 @@
+class Solution:
+    def getKth(self, lo, hi, k):
+        def power(x):
+            count = 0
+
+            while x != 1:
+                if x % 2 == 0:
+                    x //= 2
+                else:
+                    x = 3 * x + 1
+
+                count += 1
+
+            return count
+
+        arr = []
+
+        for x in range(lo, hi + 1):
+            arr.append((power(x), x))
+
+        arr.sort()
+
+        return arr[k - 1][1]
